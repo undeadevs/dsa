91cef3070c7a7ce86572f71dc64856048e12243c
Ilham <jangantanyaaku14@gmail.com>
2023-08-30 11:12:04 +0800

    Add randomized input according to node args

--- week3/practice/bubbleSort.js
+++ week3/practice/bubbleSort.js
@@ -1,15 +1,26 @@
-const arr = [1, 5, 3, 11, 7, -2, -1];
+const DIR = parseInt(parseInt(process.argv[3]));
 
+let arr = new Array(parseInt(process.argv[2]))
+    .fill(undefined)
+    .map(() => Math.floor(Math.random() * 100000) - 5000);
+
+console.log("UNSORTED: ");
 console.log(arr);
+console.log("-------------------------");
 
 for (let i = 0; i < arr.length; ++i) {
     for (let j = 0; j < arr.length - i - 1; ++j) {
-        if(arr[j]>arr[j+1]){
+        if (DIR === 0 ? arr[j] > arr[j + 1] : arr[j] < arr[j + 1]) {
             const temp = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
         }
+        console.log();
+        console.log(arr);
+        console.log();
     }
 }
 
+console.log("-------------------------");
+console.log("SORTED: ");
 console.log(arr);
